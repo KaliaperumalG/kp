8a7c8a7fc373004eb29ab548eb90ccf3e7b7b9f1
KaliaperumalG <52281474+KaliaperumalG@users.noreply.github.com>
2019-08-05 13:56:07 +0530

    Create kp31.c

---
+++ kp31.c
@@ -0,0 +1,22 @@
+#kp31
+#include <stdio.h>
+
+int main() {
+    
+    int a, n, d, tn, i;
+    int sum = 0;
+    scanf("%d", &a);
+    scanf("%d", &n);
+    scanf("%d", &d);
+    sum = (n * (2 * a + (n - 1) * d)) / 2;
+    tn = a + (n - 1) * d;
+    for(i = a; i <= tn; i = i + d)
+    {
+        if(i != tn)
+            printf("%d + ", i);
+        else
+            printf("%d", sum);
+    }
+    printf("\n");
+    return 0;
+}
